2f64eb2a9c44239879fbed70c1903c06319f2243
= <hayblec@yahoo.co.uk>
2022-03-23 23:12:56 +0000

    change the test method

--- src/test/java/com/saucedemo/tests/products/TestBackPack.java
+++ src/test/java/com/saucedemo/tests/products/TestBackPack.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 public class TestBackPack extends BaseTest {
     @Test
     public void testAddBackPack() {
-        ProductsPage productsPage = loginPage.loginWith("standard_user","secret_sauce");
+        ProductsPage productsPage = loginPage.loginWith("standard_use","secret_sauce");
 
         productsPage.addBackPack();
         Assert.assertEquals(productsPage.getButtonName(), "REMOVE");
